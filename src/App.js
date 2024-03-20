773128569ad28ac9aa10497f53146878767b2abe
xSeco <xseco@xSeco.local>
2024-03-20 17:00:38 +0200

    Almost

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import "./App.css";
 
-function App() {
+function App({ img, name, about, skills, skillBackground }) {
   return (
     <div className="App">
       <div className="card">
-        <Image img="jaguar.jpg" />
+        <div className="image1">
+          <img className="image" src={img} alt="banner" />
+        </div>
+
         <div className="half">
-          <Name name="Raul Secosan" />
-          <About
-            about=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                          veniam, quis nostrumollit anim id est laborum."
-          />
-          <Skills skills={["HTMLS + CSS", "Da"]} />
+          <div className="title">{name}</div>
+          <p className="about"> {about} </p>
+          <div className="skills" style={{ backgroundColor: skillBackground }}>
+            {skills}
+          </div>
         </div>
       </div>
     </div>
   );
-}
-
-function Image({ img }) {
-  return (
-    <div className="image1">
-      <img className="image" src={img} alt="banner" />
-    </div>
-  );
-}
-
-function Name({ name }) {
-  return <div className="title">{name}</div>;
-}
-
-function About({ about }) {
-  return <p className="about">{about}</p>;
-}
-
-function Skills({ skills }) {
-  return <div className="skills">{skills.map((skill) => skill)}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,33 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      img="jaguar.jpg"
+      name="Raul Secosan"
+      about=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrumollit anim id est laborum."
+      skills={["HTML + CSS", "JAVASCRIPT"]}
+      skillBackground="red"
+    />
+
+    <App
+      img="jaguar.jpg"
+      name="Seco"
+      about=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrumollit anim id est laborum."
+      skills={["HTML + CSS", "JAVASCRIPT"]}
+      skillBackground="yellow"
+    />
+
+    <App
+      img="jaguar.jpg"
+      name="Raul"
+      about=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrumollit anim id est laborum."
+      skills={["HTML + CSS", "JAVASCRIPT"]}
+    />
   </React.StrictMode>
 );
