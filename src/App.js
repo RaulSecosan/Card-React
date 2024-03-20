326c78f43c147ee5335704684bf3fb40be16af21
xSeco <xseco@xSeco.local>
2024-03-20 16:41:35 +0200

    Nu e buna impartirea pe componente

--- src/App.js
+++ src/App.js
@@ -4,22 +4,39 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <div className="image1">
-          <img className="image" src="jaguar.jpg" alt="banner" />
-        </div>
-
+        <Image img="jaguar.jpg" />
         <div className="half">
-          <div className="title">Raul Secosan</div>
-          <p className="about">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrumollit anim id est laborum.
-          </p>
-          <div className="skills">HTMLS + CSS</div>
+          <Name name="Raul Secosan" />
+          <About
+            about=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                          veniam, quis nostrumollit anim id est laborum."
+          />
+          <Skills skills={["HTMLS + CSS", "Da"]} />
         </div>
       </div>
     </div>
   );
+}
+
+function Image({ img }) {
+  return (
+    <div className="image1">
+      <img className="image" src={img} alt="banner" />
+    </div>
+  );
+}
+
+function Name({ name }) {
+  return <div className="title">{name}</div>;
+}
+
+function About({ about }) {
+  return <p className="about">{about}</p>;
+}
+
+function Skills({ skills }) {
+  return <div className="skills">{skills.map((skill) => skill)}</div>;
 }
 
 export default App;
