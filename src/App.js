73b2ac439c559ea3c57e64063a9e57b90c4021ca
xSeco <xseco@xSeco.local>
2024-03-20 16:31:34 +0200

    Mai trebuie impartirea pe componente

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">h1</div>;
+  return (
+    <div className="App">
+      <div className="card">
+        <div className="image1">
+          <img className="image" src="jaguar.jpg" alt="banner" />
+        </div>
+
+        <div className="half">
+          <div className="title">Raul Secosan</div>
+          <p className="about">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrumollit anim id est laborum.
+          </p>
+          <div className="skills">HTMLS + CSS</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
